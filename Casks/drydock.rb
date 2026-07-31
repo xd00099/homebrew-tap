36cadb7026a269f24dcb748e9b409910bb7a1e2a
xd00099 <xd00099@berkeley.edu>
2026-07-31 01:42:54 +0000

    drydock 0.6.3

--- Casks/drydock.rb
+++ Casks/drydock.rb
@@ -1,6 +1,6 @@
 cask "drydock" do
-  version "0.6.2"
-  sha256 "4f866eb20e1a1c18b473f97ddb950ba4793fbc831de4edd79daf6c1ab11a72c4"
+  version "0.6.3"
+  sha256 "b0e0a576edeb36680483dba62e5efe66155ab4ceaadf2846ed4bb2fb66c9e7b1"
 
   url "https://github.com/xd00099/drydock/releases/download/v#{version}/Drydock_#{version}_aarch64.dmg"
   name "Drydock"
